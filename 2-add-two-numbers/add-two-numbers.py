0dd2b461616695cf2affd9a8fed7e33547bab03b
Muhammad Qasim <123858146+qasim-you@users.noreply.github.com>
2025-07-27 23:10:50 +0500

    Update add-two-numbers.py

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -9,7 +9,7 @@ class Solution(object):
         current = head
         carry = 0
         while (l1 != None or l2 != None or carry != 0):
-            l1_value = l1.val if l1 else 0
+            l1_value = l1.val if l1 else 0 
             l2_value = l2.val if l2 else 0
             total = l1_value + l2_value + carry
             current.next = ListNode(total % 10)
